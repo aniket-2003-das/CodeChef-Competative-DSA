63d4fcfd04199261e502719a4324859ffbff4234
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-10-01 06:49:53 +0530

    203

---
+++ Cricket World Cup Qualifier.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(int n) {
+    if (n>=12) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
+
+int main() {
+	// your code goes here
+	int X;
+	cin>>X;
+    Paa(X);
+}
