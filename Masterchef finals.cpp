bc4503eda3e52fd9953c85299ee9b8d390e357e1
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-10-09 14:41:08 +0530

    255

---
+++ Masterchef finals.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void Paa() {
+    int X;
+    cin >> X;
+    if (X <= 10) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+
+int main() {
+    // your code goes here
+    int T;
+    cin >> T;
+    while (T--) Paa();
+}
