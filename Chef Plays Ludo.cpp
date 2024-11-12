12e0b14fa0bd6f7b93de051231cb5c4a52795ca9
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-12 20:20:57 +0530

    260

---
+++ Chef Plays Ludo.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X;
+    cin >> X;
+    if (X==6) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	while(T--) Paa();
+}
