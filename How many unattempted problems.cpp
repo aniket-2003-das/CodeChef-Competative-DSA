aca13a00e8f1b77930a534c565d69b876c19ae06
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-13 18:26:52 +0530

    264

---
+++ How many unattempted problems.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(int X, int Y){
+    cout<<X-Y<<endl;
+}
+
+int main() {
+	// your code goes here
+	int X, Y;
+    cin>>X>>Y;
+    Paa(X, Y);
+    return 0;
+}
