4a04983fb89e87e27ff9a1fa1f77ac99ecdf957c
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-16 02:01:07 +0530

    279

---
+++ Audible Range.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa() {
+    int X;
+    cin>>X;
+    if(X>=67 && X<=45000) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
