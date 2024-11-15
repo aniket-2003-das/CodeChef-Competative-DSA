554828b76503b96493a45cef75a624988c159970
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-15 18:33:41 +0530

    272

---
+++ Donation Drive.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int N,X;
+	cin>>N>>X;
+    cout<<(N-X)<<endl;
+}
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+    while(T--) Paa();
+}
