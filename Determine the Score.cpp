e7eed9b6fdfd83d84b32aeb978299859713e0388
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-15 15:05:33 +0530

    267

---
+++ Determine the Score.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X, N;
+    cin>>X>>N;
+    cout<<(X/10)*N<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
