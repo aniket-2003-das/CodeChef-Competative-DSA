bd50020bfe009e32a8f199c5b4710e5404a2014d
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-17 15:59:51 +0530

    296

---
+++ Total Prize Money.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X,Y;
+    cin>>X>>Y;
+    cout<<(10*X)+((90*Y))<<endl;
+}
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+    while(T--) Paa();
+}
