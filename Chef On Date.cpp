0814d2f14c5f32a41f25cdec2008df1f882e7fd2
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-17 15:50:34 +0530

    294

---
+++ Chef On Date.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Paa(){
+    int X,Y;
+    cin>>X>>Y;
+    if((X-Y)<0) cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
