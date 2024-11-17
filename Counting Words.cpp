75d2521fe24e3c766c44bcf9d14be1a371a98c16
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-17 16:02:10 +0530

    296

---
+++ Counting Words.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int N,M;
+    cin>>N;
+    cin>>M;
+    cout<<(N*M)<<endl;
+}
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+    while(T--) Paa();
+}
