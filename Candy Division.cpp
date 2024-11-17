f454b40d64c009aadb01958bc9687e7ecb1e0496
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-17 14:45:56 +0530

    289

---
+++ Candy Division.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int N;
+    cin>>N;
+    if((N%3)==0) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
