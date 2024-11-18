17548184713b3e35967565805771e26a44568dde
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-18 19:48:40 +0530

    302

---
+++ Four Tickets.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int X;
+	    cin>>X;
+	    if(4*X>1000) cout<<"NO"<<endl;
+	    else cout<<"YES"<<endl;
+	}
+}
