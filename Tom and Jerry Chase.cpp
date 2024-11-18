401137fe17f8bea9828fd2dc1d1cc10d7b870349
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-18 05:33:01 +0530

    298

---
+++ Tom and Jerry Chase.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X,Y;
+    cin>>X>>Y;
+    if(Y>X) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--) Paa();
+}
