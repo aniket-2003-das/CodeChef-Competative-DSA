05d084417ed646eb21e85291b4824478de27179c
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-18 05:47:33 +0530

    299

---
+++ Ageing.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X;
+    cin>>X;
+    cout<<10+(X-20)<<endl;
+}
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--) Paa();
+}
