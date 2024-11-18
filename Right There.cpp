96a463294b8bc859ea7ced44633cba88143e3525
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-18 05:50:43 +0530

    299

---
+++ Right There.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int N,X;
+    cin>>N>>X;
+    if(X<N) cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
