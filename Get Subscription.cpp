02486e697d475c0df20c58e28cf13a5d3acb8d59
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-19 19:52:56 +0530

    315

---
+++ Get Subscription.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X;
+    cin>>X;
+    if(X>30) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
