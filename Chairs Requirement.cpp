27005f21649581b88945ab8a436583b5c627a3d8
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-19 05:56:16 +0530

    305

---
+++ Chairs Requirement.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X, Y;
+    cin>>X>>Y;
+    if(X>Y) cout<<X-Y<<endl;
+    else cout<<0<<endl;
+}
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--) Paa();
+}
