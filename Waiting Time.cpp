d9b3466894be30bfda092d4529adbb64331129ff
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-19 20:04:13 +0530

    319

---
+++ Waiting Time.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa() {
+    int K, X;
+    cin>>K;
+    cin>>X;
+    cout<<7*K - X<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
