9cf9b3039e7e7d52e2c44fb7d05c657ad377a6ae
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-19 19:49:16 +0530

    308

---
+++ Sum it.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa() {
+    int A, B, C;
+    cin>>A>>B>>C;
+    if (A+B==C) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
