59342f31cdff54bee7e94aa2292d192870e69829
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-20 06:29:09 +0530

    342

---
+++ Passes for Fair.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int K, N;
+    cin>>N>>K;
+    if(K>N) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
