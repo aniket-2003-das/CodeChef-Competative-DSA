e2d1af0bdc19a69f0c6b94e6bf21f96badda1b20
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-21 21:22:19 +0530

    350

---
+++ Chef and Chapters.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X, Y, Z;
+    cin>>X>>Y>>Z;
+    cout<<Y*X*Z<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
