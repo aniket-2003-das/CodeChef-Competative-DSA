b37958065ed126a34b941403fe80d94ac6eff546
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-21 21:14:26 +0530

    348

---
+++ Sleep deprivation.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Paa(){
+    int X;
+    cin>>X;
+    if(X<7) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--) Paa();
+}
