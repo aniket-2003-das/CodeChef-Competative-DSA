8275e65cfc8b3832e6095d283e44327434752de3
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-22 02:14:21 +0530

    352

---
+++ Lunchtime.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int X;
+	    cin>>X;
+	    if(X>=1 && X<=4) cout<<"YES"<<endl;
+	    else cout<<"NO"<<endl;
+	}
+}
