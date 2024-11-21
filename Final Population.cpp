7fcaca8d0483e7b7e90f0c029f1f06a044b39bb3
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-22 03:00:01 +0530

    358

---
+++ Final Population.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Paa(){
+    int X, Y, Z;
+    cin>>X>>Y>>Z;
+    cout<<X-Y+Z<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
