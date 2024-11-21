dbb49f34f96fa6595d1223aac16f9068154ebb05
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-21 20:49:51 +0530

    347

---
+++ Couple Game.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    int B, G;
+    while(T--){
+        cin>>G>>B;
+        cout<<B-G<<endl;
+    }
+}
