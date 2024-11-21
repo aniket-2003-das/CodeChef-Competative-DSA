03346575b3faa88ada07c71046452a40af894a5a
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-22 03:15:38 +0530

    363

---
+++ Chef gives Party.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa() {
+    int N, X, K;
+    cin>>N>>X;
+    cin>>K;
+    if(N*X>K) cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
