1af85aeb534576943273561b070ebcbea203b9f3
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-21 21:11:23 +0530

    348

---
+++ Fever.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X;
+    cin>>X;
+    if(X>98) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+    while(T--) Paa();
+}
