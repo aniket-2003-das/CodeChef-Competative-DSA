6fc1ebffab5d2c9a3919e11684675dd5de4c0317
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-21 21:19:33 +0530

    349

---
+++ MATH1 Enrolment.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X, Y;
+    cin>>X>>Y;
+    if(Y-X>0) cout<<Y-X<<endl;
+    else cout<<0<<endl;
+     
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
+
