c5e71f068204705d6da39e6d4b0e84a44868d094
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-22 05:34:33 +0530

    364

---
+++ Time Complexity.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X, Y;
+    cin>>X>>Y;
+    if(X>Y) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
