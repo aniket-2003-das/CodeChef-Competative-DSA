2dfc74aacf6586b9ce52a2f8184c8410f0982ab1
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-23 06:08:55 +0530

    376

---
+++ Multivitamin Tablets.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--){
+        int X, Y;
+        cin>>X;
+        cin>>Y;
+        if(3*X<=Y) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
