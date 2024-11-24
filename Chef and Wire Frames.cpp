3a5fb6faf3d34d53f5a3cddb62bfb8d6e275a571
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-24 05:54:36 +0530

    383

---
+++ Chef and Wire Frames.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int N, M, X;
+    cin>>N>>M>>X;
+    cout<<2*(N+M)*X<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
