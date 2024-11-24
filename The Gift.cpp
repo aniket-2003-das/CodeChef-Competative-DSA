329fde457c1bb9764cbc7118d70014ba4a8f7078
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-24 07:18:39 +0530

    390

---
+++ The Gift.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int X, N, M;
+    cin>>X>>N>>M;
+    if(X+M>=N) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
