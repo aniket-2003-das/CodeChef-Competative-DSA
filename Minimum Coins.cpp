d1356501902d59c17c2752c76f8358145cfcf833
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-24 06:17:26 +0530

    390

---
+++ Minimum Coins.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X;
+    cin>>X;
+    cout<<X%10<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
