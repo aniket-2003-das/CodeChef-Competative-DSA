617e087a7f0ac6a6c15c1a30834135af78914043
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-24 09:21:08 +0530

    399

---
+++ Discount.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int x;
+    cin>>x;
+    cout<<(100-x)<<endl;
+}
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
