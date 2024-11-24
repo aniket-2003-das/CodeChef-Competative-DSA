4653d1f80865c4962d24b4e88e84f4439524a801
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-24 09:09:31 +0530

    398

---
+++ True and False Paper.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int N, K;
+    cin>>N;
+    cin>>K;
+    cout<<N-K<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
