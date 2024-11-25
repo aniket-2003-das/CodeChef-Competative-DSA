73a5afa94515d746cc734442b7a3c650e647bfd0
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-26 00:30:11 +0530

    409

---
+++ Volume Control.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X, Y;
+    cin>>X>>Y;
+    cout<<max(X, Y)-min(X, Y)<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
