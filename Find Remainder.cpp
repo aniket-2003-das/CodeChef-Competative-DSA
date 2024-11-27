f9f7eb3975472a5e7141997f7353536c44dc181e
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-27 10:25:15 +0530

    421

---
+++ Find Remainder.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X, N;
+    cin>>N>>X;
+    cout<<N%X<<endl;
+}
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--) Paa();
+}
