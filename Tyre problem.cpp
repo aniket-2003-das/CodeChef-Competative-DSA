223248db7a3287996e64eaba89ebcb0bfd8f351f
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-28 15:01:12 +0530

    452

---
+++ Tyre problem.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int  T, N, M;
+    cin>>T;
+    while(T--) {
+        cin>>N>>M;
+        cout<<2*N + 4*M<<endl;
+    }
+}
