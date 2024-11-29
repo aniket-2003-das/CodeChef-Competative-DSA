3b115aa7f546ded2a8e88e7fdb2837142fbf302d
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-29 07:41:08 +0530

    479

---
+++ Battery Low.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Paa(){
+    int X;
+    cin>>X;
+    if(X<=15) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
