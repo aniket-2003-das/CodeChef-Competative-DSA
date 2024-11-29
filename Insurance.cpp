e5b068a9376fe58e5603af89cd07fc8b1930da0a
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-29 07:22:02 +0530

    475

---
+++ Insurance.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X, Y;
+    cin>>X>>Y;
+    if(Y<=X) cout<<Y<<endl;
+    else cout<<X<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
