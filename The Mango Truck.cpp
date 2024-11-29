37a0e1a9eb6b0f48ee678d8f70b22a5c3d0e9856
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-29 07:48:54 +0530

    482

---
+++ The Mango Truck.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Paa(){
+    int X, Y, Z;
+    cin>>X>>Y>>Z;
+    cout<<(Z-Y)/X<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
+
