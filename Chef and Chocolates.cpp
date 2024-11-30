ee248fa4d2405c465e5fe8d22983ad512e9376fb
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-11-30 10:19:43 +0530

    492

---
+++ Chef and Chocolates.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X, N, Y;
+    cin>>N;
+    cin>>X;
+    cin>>Y;
+    cout<<(N-X)*Y<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
