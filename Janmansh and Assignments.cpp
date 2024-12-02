8e5d5fdb060763d9859c95563f7e612af9b8c295
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-12-02 19:08:27 +0530

    513

---
+++ Janmansh and Assignments.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa() {
+    int N;
+    cin>>N;
+    if(N<8) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
