77e9efd0b1da22ea06ff6758510ebd672be268fe
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-12-03 15:00:08 +0530

    532

---
+++ Chef in his Office.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X, Y;
+    cin>>X;
+    cin>>Y;
+    cout<<4*X+Y<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
