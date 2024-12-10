503a32b61ad94ddcb40411f088fe14a68129c719
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-12-10 21:36:55 +0530

    546

---
+++ Minimum Pizzas.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    double N,X;
+    cin>>N>>X;
+    cout<<ceil((N*X)/4)<<endl;
+    
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
