53eb76587a602bdf63f910dde61ec19995488c47
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-12-10 23:11:47 +0530

    550

---
+++ Chefland Games.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int r1, r2, r3, r4;
+    cin>>r1>>r2>>r3>>r4;
+    if(r1==0 && r2==0 && r3==0 && r4==0) cout<<"IN"<<endl;
+    else cout<<"OUT"<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
