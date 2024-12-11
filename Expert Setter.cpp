774d76707f1ea52df959263a07c8c14e61669104
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-12-12 05:19:27 +0530

    561

---
+++ Expert Setter.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    double Y, X;
+    cin>>X>>Y;
+    if(X/2>Y) cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
