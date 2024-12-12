11a829a24efd719a9222cfc539dd7e424a2e9bb5
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-12-12 05:51:26 +0530

    563

---
+++ Sugarcane Juice Business.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int A;
+    cin>>A;
+    cout<<15*A<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
