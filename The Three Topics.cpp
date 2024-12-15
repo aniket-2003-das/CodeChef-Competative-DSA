84e701485edb24fbf839d833c368208a990eda25
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-12-15 13:40:30 +0530

    573

---
+++ The Three Topics.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int A, B, C, D;
+    cin>>A>>B>>C>>D;
+    if(D==A || D==B || D==C) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
