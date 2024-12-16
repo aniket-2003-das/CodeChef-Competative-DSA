827388d809fbddad3085962c9bb7acbc262c5eab
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-12-16 11:58:48 +0530

    585

---
+++ Nearest Exit.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int A;
+    cin>>A;
+    if(A>50) cout<<"RIGHT"<<endl;
+    else cout<<"LEFT"<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
