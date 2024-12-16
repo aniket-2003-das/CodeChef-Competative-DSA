5cfd439a68fc3128cf91b78369c41a0ce6e3ae3a
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-12-16 15:22:01 +0530

    593

---
+++ Good Program.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int A;
+    cin>>A;
+    if(A%4==0) cout<<"Good"<<endl;
+    else cout<<"Not Good"<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
