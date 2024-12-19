7491f6d2b177c827f4898870a2190a736548a9a3
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-12-19 16:57:19 +0530

    623

---
+++ Discus Throw.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int A, B, C;
+    cin>>A>>B>>C;
+    cout<<max({A, B, C})<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
