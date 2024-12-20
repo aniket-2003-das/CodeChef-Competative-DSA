200a6af515ae437df976ee412c09b4bb26c8de6c
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-12-20 14:22:57 +0530

    643

---
+++ Bath in Winters.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X, Y;
+    cin>>X>>Y;
+    cout<<X/(2*Y)<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
