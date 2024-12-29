e6cb045bb7685de703ac34f6cc29cd39efb99bc5
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2024-12-30 01:06:33 +0530

    668

---
+++ Finding Square Roots.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int N;
+    cin>>N;
+    cout<<floor(sqrt(N))<<endl;
+}
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
