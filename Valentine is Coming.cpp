b246cb4f9afb827f9049f79f89ef795212e1ff7f
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2025-01-03 11:29:16 +0530

    691

---
+++ Valentine is Coming.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int X1, Y1;
+    cin>>X1>>Y1;
+    cout<<X1/Y1<<endl;
+}
+
+int main() {
+// 	your code goes here
+    int T;
+    cin>>T;
+    while(T--) Paa();
+}
