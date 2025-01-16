9ad988e3660081a369408fa2b410b1ba7b778457
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2025-01-16 14:39:29 +0530

    722

---
+++ Decrement OR Increment.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    if(T%4==0) cout<<++T<<endl;
+    
+    else cout<<--T<<endl;
+}
