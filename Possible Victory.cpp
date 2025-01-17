d10f091a56d67f6e490b42ddc065c14a38510cc7
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2025-01-17 22:06:43 +0530

    769

---
+++ Possible Victory.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	double R, O, C;
+	cin>>R>>O>>C;
+    if((R-C)/(20-O)>=36) cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
+}
