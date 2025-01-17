ef91ec954d5728edd1356cc1ca2413c34fa67c57
aniket-2003-das <150231611+aniket-2003-das@users.noreply.github.com>
2025-01-17 21:32:48 +0530

    763

---
+++ Dracula Eats.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Paa(){
+    int N;
+    cin>>N;
+    cout<<(N+5)/7<<endl;
+}
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--) Paa();
+}
